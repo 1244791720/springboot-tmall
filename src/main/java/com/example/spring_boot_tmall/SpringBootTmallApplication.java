c40f1fa67c504e6bf2dbeb41ec1dc0c630764f72
1244791720 <36809066+1244791720@users.noreply.github.com>
2018-10-24 11:24:01 +0800

    firstcommit

---
+++ src/main/java/com/example/spring_boot_tmall/SpringBootTmallApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_tmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTmallApplication.class, args);
+    }
+}
